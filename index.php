7d8973a4742a027d25e9a16b4e6413876e22da31
Jeff <jmsalazar@dswd.gov.ph>
2024-02-19 03:49:42 +0800

    design login

--- index.php
+++ index.php
@@ -1,67 +1,137 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login Page</title>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            background-color: #f4f4f4;
-            margin: 0;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-        }
-
-        .container {
-            background-color: #fff;
-            padding: 20px;
-            border-radius: 5px;
-            box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
-            width: 300px;
-        }
-
-        .container h2 {
-            text-align: center;
-        }
-
-        .container input[type="text"],
-        .container input[type="password"] {
-            width: 100%;
-            padding: 10px;
-            margin: 8px 0;
-            box-sizing: border-box;
-            border: 1px solid #ccc;
-            border-radius: 4px;
-        }
-
-        .container input[type="submit"] {
-            width: 100%;
-            background-color: #4caf50;
-            color: white;
-            padding: 10px 0;
-            margin: 8px 0;
-            border: none;
-            border-radius: 4px;
-            cursor: pointer;
-        }
-
-        .container input[type="submit"]:hover {
-            background-color: #45a049;
-        }
-    </style>
+<?php
+  session_start();
+	$_SESSION["token"] = bin2hex(random_bytes(32));
+	$_SESSION["token-expire"] = time() + 3600; // 1 hour = 3600 secs
+// include 'includes/conn_to_ctris.php';
+require_once('includes/init.php');
+include 'modal/registermodal.php';
+   include 'includes/functions.php'; 
+?>
+  <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+  <script src="includes/scripts.js" async defer></script>
+  
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <link rel="shortcut icon" href="images/dtricon.jpg">
+    <title>Registration Module</title>
+  <!-- Tell the browser to be responsive to screen width -->
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+  <!-- Bootstrap 3.3.7 -->
+  <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
+  <!-- Select2 -->
+  <link rel="stylesheet" href="bower_components/bootstrap-daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="bower_components/select2/dist/css/select2.min.css">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
+  <!-- AdminLTE Skins. Choose a skin from the css/skins
+       folder instead of downloading all of them to reduce the load. -->
+  <link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
+  <!-- Google Font -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+  <link rel="stylesheet" href="plugins/iCheck/all.css">
+  <link rel="stylesheet" href="includes/add.css">
+   <!-- Bootstrap Color Picker -->
+   <link rel="stylesheet" href="bower_components/bootstrap-colorpicker/dist/css/bootstrap-colorpicker.min.css">
+     <!-- Bootstrap time Picker -->
+  <link rel="stylesheet" href="plugins/timepicker/bootstrap-timepicker.min.css">
+  <style>
+    .requiredField{
+      color:red;
+      font-weight: bold;
+      font-size: 18px;
+    }
+  </style>
 </head>
-<body>
-    <div class="container">
-        <h2>Login</h2>
-        <form action="login.php" method="post">
-            <input type="text" name="username" placeholder="Username" required>
-            <input type="password" name="password" placeholder="Password" required>
-            <input type="submit" value="Login">
-        </form>
+
+<!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+<body class="hold-transition skin-blue layout-top-nav">
+<div class="wrapper">
+<?php include 'includes/header.php';?>
+  <!-- Full Width Column -->
+  <div class="content-wrapper">
+    <div class="container-fluid">
+      <!-- Main content -->
+      <section class="content" style="width: 40% ;">
+            <div class="box" id="SearchContent">
+                  <form class="form-horizontal" method="POST" id="contentsearch">
+                    <div class="box-body">
+                    <h3 class="box-title">Login</h3>
+                      <div class="row">
+                        <div class="col-md-2">
+                        
+                        </div>
+                        <div class="col-md-12">
+                        <label for="Search" class="col-sm-12">Employee Number</label>
+                          <div class="input-group col-sm-12">
+                            <div class="input-group-addon">
+                              <i>
+                                <label style="font-size: 15px; margin:auto" for="03-">03-</label>
+                              </i>
+                            </div>
+                            <input type="text" class="form-control" name="txtSearch" id="txtSearch" placeholder="Employee Number" value="" style="text-transform: uppercase;" required="true" onkeypress="return NumberOnly(event)" tabindex="1">
+                          </div>
+                          <small id='CheckIDmessage'></small>
+                        </div>
+                      </div>
+                      <br>
+                          <div class="row">
+                            <div class="col-md-12">
+                              <button type="submit" class="form-control btn btn-info" id="btn_search" name="btn_search">
+                                SEARCH 
+                              <i class="fa fa-search"></i>
+                              </button>
+                            </div>
+                          </div>
+                    </div>
+                  </form>
+                </div>
+      </section>
+      <!-- /.content -->
     </div>
+    <!-- /.container -->
+  </div>
+  <!-- /.content-wrapper -->
+  
+  
+</div>
+
+<!-- Start Scripts calling -->
+<!-- ./wrapper -->
+<!-- jQuery 3 -->
+<script src="bower_components/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap 3.3.7 -->
+<script src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- AdminLTE App -->
+<script src="dist/js/adminlte.min.js"></script>
+<script src="bower_components/select2/dist/js/select2.full.min.js"></script>
+<!-- SlimScroll -->
+<script src="bower_components/jquery-slimscroll/jquery.slimscroll.min.js"></script>
+<!-- FastClick -->
+<script src="bower_components/fastclick/lib/fastclick.js"></script>
+<script src="plugins/input-mask/jquery.inputmask.js"></script>
+<script src="plugins/input-mask/jquery.inputmask.date.extensions.js"></script>
+<script src="plugins/input-mask/jquery.inputmask.extensions.js"></script>
+<script src="bower_components/bootstrap-daterangepicker/daterangepicker.js"></script>
+<script src="bower_components/moment/min/moment.min.js"></script>
+<!-- bootstrap datepicker -->
+<script src="bower_components/bootstrap-datepicker/dist/js/bootstrap-datepicker.min.js"></script>
+<!-- AdminLTE for demo purposes -->
+<script src="dist/js/demo.js"></script>
+<!-- iCheck 1.0.1 -->
+<script src="plugins/iCheck/icheck.min.js"></script>
+<!-- bootstrap color picker -->
+<script src="bower_components/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
+<!-- bootstrap time picker -->
+<script src="plugins/timepicker/bootstrap-timepicker.min.js"></script>
+<script src="pluginscript.js"></script>
+<!-- End Scripts calling -->
+
 </body>
 </html>
